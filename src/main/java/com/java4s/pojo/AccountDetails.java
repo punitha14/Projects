ab2bce69cdc2c54d71d9dd0e40d4ec88a9ce7e71
punitha14 <punitha14.av@gmail.com>
2021-06-17 10:32:08 +0530

    comment

---
+++ src/main/java/com/java4s/pojo/AccountDetails.java
@@ -0,0 +1,48 @@
+package com.java4s.pojo;
+
+import java.io.Serializable;
+
+public class AccountDetails implements Serializable {
+
+	    private static final long serialVersionUID = -3760445487636086034L;
+	    private String accountNo;
+	    private String accountName;
+	    private String address;
+	    private String accountStatus;
+	    private double depositAmount;
+	    
+		
+		public double getDepositAmount() {
+			return depositAmount;
+		}
+		public void setDepositAmount(double depositAmount) {
+			this.depositAmount = depositAmount;
+		}
+		public String getAccountNo() {
+			return accountNo;
+		}
+		public void setAccountNo(String accountNo) {
+			this.accountNo = accountNo;
+		}
+		public String getAccountName() {
+			return accountName;
+		}
+		public void setAccountName(String accountName) {
+			this.accountName = accountName;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getAccountStatus() {
+			return accountStatus;
+		}
+		public void setAccountStatus(String accountStatus) {
+			this.accountStatus = accountStatus;
+		}
+
+	   
+
+}
